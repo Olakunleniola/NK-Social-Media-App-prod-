1b003540f4acbb9ff1c0981760c6fffd9ea0d347
olakunle <akandebaba48@gmail.com>
2024-02-04 18:36:54 +0100

    created a followers model and reqisted it in the app's admin

--- followers/admin.py
+++ followers/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Followers
 
-# Register your models here.
+class FollowersAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Followers, FollowersAdmin)
--- followers/models.py
+++ followers/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+from django.db import models, IntegrityError
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Followers(models.Model): 
+    followed_by = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name = "followed_by"
+    )
+    
+    following = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name = "following"
+    )
+    
+    def __str__(self):
+        return f"{self.followed_by.username} is following {self.following.username}"
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['followed_by', 'following'], name='unique_following'),
+            models.CheckConstraint(check=~models.Q(followed_by=models.F('following')), name='check_following_not_same_user')
+        ]
