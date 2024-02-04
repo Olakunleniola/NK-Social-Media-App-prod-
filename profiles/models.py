b667fa38a99a6187c3e1dcbd116d93fbaad3f80c
olakunle <akandebaba48@gmail.com>
2024-02-04 18:28:04 +0100

    created templates html to render the created profiles app viewa, and also registered their url

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from sorl.thumbnail import ImageField
 
 # Create your models here.
+class Profile(models.Model) : 
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        # related_name = 'profile'
+    )
+    image = ImageField(upload_to='profile_images')
+    backdrop_image = ImageField(upload_to='profile_images/', null=True, blank=True)
+    
+    def __str__(self) : 
+        return f'{self.user.username} {self.user.last_name}'
+    
+@receiver(post_save, sender=User)
+def create_user_profile (sender, instance, created, **kwargs) : 
+    """Create a new Profile() object whenever a django user is created"""
+    if created: 
+        Profile.objects.create(user=instance)
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,142 @@
+from typing import Any
+from django.contrib.auth.models import User
+from django.views.generic import DetailView, View, CreateView, TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import JsonResponse, HttpResponseBadRequest, HttpResponseRedirect
+from django.urls import reverse
+from feed.models import Post
 from django.shortcuts import render
+from followers.models import Followers
+from .models import Profile
 
-# Create your views here.
+class ProfileDetailView( DetailView ):
+    http_method_names = ["get"]
+    template_name = "profiles/details.html"
+    model = User
+    context_object_name = "user"
+    slug_field = "username"
+    slug_url_kwarg = "username"
+    
+    def dispatch(self, request, *args, **kwargs) :
+        self.request = request
+        return super().dispatch(request, *args, **kwargs)
+    
+    def get_context_data(self, **kwargs):
+        user = self.get_object()
+        context = super().get_context_data(**kwargs)
+        context["total_posts"] = Post.objects.filter(author=user).count()
+        context["total_followers"] = Followers.objects.filter(following__username=user.username).count()
+        context["total_following"] = Followers.objects.filter(followed_by__username=user.username).count()
+        if self.request.user.is_authenticated:
+            context["check_follow"] = Followers.objects.filter(following=user, followed_by=self.request.user).exists()
+        return context
+    
+class FollowView(LoginRequiredMixin, View):
+    http_method_names = ["post"]
+    
+    def post(self, request, *args, **kwargs):
+        data = request.POST.dict()
+        
+        if "username" not in data or "action" not in data:
+            return HttpResponseBadRequest("Missing Data")
+        
+        try: 
+            other_user = User.objects.get(username=data['username'])
+        except User.DoesNotExist:
+            return HttpResponseBadRequest("Missing User")
+        
+        if data['action'] == "Follow":
+            #follow
+            follower, created = Followers.objects.get_or_create(
+                followed_by = request.user, #this is the logged in user
+                following = other_user
+            )
+        else: 
+            #unfollow
+            try: 
+                follower = Followers.objects.get(
+                    followed_by = request.user,
+                    following = other_user
+                )
+            except Followers.DoesNotExist:
+                follower = None
+                
+            if follower: 
+                follower.delete()
+        
+        follower_size = Followers.objects.filter(following__username=other_user).count()
+        following_size = Followers.objects.filter(followed_by = other_user).count()
+        
+        return JsonResponse({
+            "done": "Success",
+            "wording": "Unfollow" if data["action"] == "Follow" else "Follow",
+            "followers": follower_size,
+            "following": following_size,
+        }) 
+        
+        
+class EditProfile(LoginRequiredMixin, TemplateView): 
+    http_method_names = ["get","post"]
+    template_name = "profiles/edit_profile.html"
+
+    def post(self, request, *args, **kwargs):
+        profile_image = request.FILES.get("image")
+        cover_image = request.FILES.get("cover_image")
+        username = request.POST.get("username")
+        first_name =  request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        email = request.POST.get("email")
+        print(username)
+        print(first_name)
+        print(last_name)
+        print(email)
+
+        # Validate images
+        if profile_image: 
+            if not self.verify_image(profile_image):
+                return JsonResponse({
+                    'error': 'Invalid profile image. Only JPEG, PNG, and GIF files are allowed.'
+                }, status=400)
+            request.user.profile.image = profile_image
+
+        if cover_image:    
+            if not self.verify_image(cover_image):
+                return JsonResponse({
+                    'error': 'Invalid cover image. Only JPEG, PNG, and GIF files are allowed.'
+                }, status=400)
+            request.user.profile.backdrop_image = cover_image
+                 
+        # Update user profile and user details
+        if username:
+            request.user.username = username
+        
+        if first_name: 
+            request.user.first_name = first_name
+        
+        if last_name:
+            request.user.last_name = last_name
+        
+        if email:
+            request.user.email = email
+        
+        # Save the changes to the user
+        request.user.save()
+
+        # Update or set profile images
+        request.user.profile.save()
+
+        # Redirect to the user's profile page
+        redirect_url = reverse('profiles:detail', kwargs={'username': request.user.username})
+        response_data = {'redirect': redirect_url}
+        return JsonResponse(response_data)
+        
+
+    @staticmethod
+    def verify_image(image_file):
+        allowed_types = ['image/jpeg', 'image/png', 'image/gif']
+
+        # Check the content type of the image
+        if image_file.content_type not in allowed_types:
+            return False
+
+        return True
